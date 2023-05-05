1aa3c349d40dd715f09070ab679d5d3ebfa51423
isa-bolanos <isa.bolanos55@gmail.com>
2023-05-05 17:57:32 -0500

    Initial Commit

---
+++ assignment notebook2/assignment_notebook2App.swift
@@ -0,0 +1,17 @@
+//
+//  assignment_notebook2App.swift
+//  assignment notebook2
+//
+//  Created by isa bolanos on 5/5/23.
+//
+
+import SwiftUI
+
+@main
+struct assignment_notebook2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
