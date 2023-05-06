9089567d81e14927017c86a503c93a53723e34ad
isa-bolanos <isa.bolanos55@gmail.com>
2023-05-05 21:02:28 -0500

    Created additional features to AddItemsView

--- assignment notebook2/ContentView.swift
+++ assignment notebook2/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     @ObservedObject var toDoList = ToDoList()
-
+@State private var showingAddItemView = false
     var body: some View {
         NavigationView {
             List {
@@ -30,9 +30,16 @@ struct ContentView: View {
     .onDelete { indexSet in
         toDoList.items.remove(atOffsets: indexSet)
     }
-            }
+       }
+            .sheet(isPresented: $showingAddItemView, content: {
+                AddItemView(toDoList: toDoList)
+            })
             .navigationBarTitle("To Do List")
-            .navigationBarItems(leading: EditButton())
+            .navigationBarItems(leading: EditButton(),
+                                trailing: Button(action: {
+                showingAddItemView = true}) {
+                    Image(systemName: "plus")
+                })
         }
     }
 }
